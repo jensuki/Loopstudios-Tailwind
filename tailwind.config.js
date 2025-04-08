adb9d635cbdd53456662c44c1f48b64297b74b3b
jensuki <jeni.suk@proton.me>
2025-04-07 20:38:52 -0700

    initial commit with config setup for screen sizes + fonts

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '758px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin', 'sans-serif'],
+        alata: ['Alata']
+      }
+    },
+  },
+  plugins: [],
+}
